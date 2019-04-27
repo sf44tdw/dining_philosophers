a867ca3ca2d9860c1f03ebcf6ed56e84b84e2918
sf44tdw <sf44tdw@users.noreply.github.com>
2019-04-27 14:21:55 +0900

    変数名を変えろと言われたので変えた。

--- src/philosopher.rs
+++ src/philosopher.rs
@@ -13,26 +13,26 @@ pub struct Philosopher {
 }
 
 impl Philosopher {
-   pub fn new(name: &str, left: usize, right: usize) -> Philosopher {
+   pub fn new(name: &str, p_left: usize, p_right: usize) -> Philosopher {
         Philosopher {
             name: name.to_string(),
-            left: left,
-            right: right,
+            left: p_left,
+            right: p_right,
         }
     }
 
    pub fn eat(&self, table: &Table) {
         let _left = table.forks[self.left].lock().unwrap();
-        
+
         let taking_time = thread_rng().gen_range(1, 150);
-        
+
         println!("{} is taking. {} ms.", self.name, taking_time);
-        
+
         thread::sleep(Duration::from_millis(taking_time));
         let _right = table.forks[self.right].lock().unwrap();
 
         let eating_time = thread_rng().gen_range(1, 1000);
-        
+
         println!("{} is start eating. {} ms.", self.name, eating_time);
 
         thread::sleep(Duration::from_millis(eating_time));
